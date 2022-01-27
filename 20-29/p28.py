04a37084265f1b862c643dd1373059392498b8d6
Earthend <jeanluc.williams@protonmail.com>
2022-01-26 19:40:56 -0800

    Solved problem 28

---
+++ 20-29/p28.py
@@ -0,0 +1,23 @@
+#!usr/bin/env python3
+
+def sumDiagonals(n):
+    num = list(range(1, n*n + 1))
+    index = 0
+    total = 1
+    numbers_added = 1
+    corner_count = 1
+    layer = 1
+    while numbers_added < n * 2 - 1:
+        index += 2 * layer
+        total += num[index]
+        numbers_added += 1
+        corner_count += 1
+        if corner_count > 4:
+            corner_count = 1
+            layer += 1
+    print(total)
+
+
+
+if __name__ == "__main__":
+    main(1001)
